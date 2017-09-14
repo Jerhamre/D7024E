925a59f159b4e48823598757315acbe291172c36
Jerhamre <anton.jerhamre@gmail.com>
2017-09-14 09:08:21 +0200

    fixed bugs in lLookupContact

--- kademlia/kademlia.go
+++ kademlia/kademlia.go
@@ -1,37 +1,33 @@
 package kademlia
 
-import (
-	"container/heap"
-	"fmt"
-)
-
 type Kademlia struct {
-	routingTable *RoutingTable
-  network *Network
+	RoutingTable 	RoutingTable
+  Network 			Network
 }
 
 func (kademlia *Kademlia) LookupContact(target *Contact) {
 
-	const alpha := 3
-	const k := 20
+	const alpha = 3
+	const k = 20
 
 	var contacts []Contact
-	var seenIDs []KademliaID
+	var seenIDs []*KademliaID
 
-	done = make(chan []Contact)
+	done := make(chan []Contact)
 	returned_contacts := make([]Contact, bucketSize)
 
   //pick out alpha (3) nodes from its closest non-empty k-bucket
-  returned_contacts = append(returned_contacts, routingTable.FindClosestContacts(target.ID, alpha))
+	returned_contacts = append(returned_contacts, kademlia.RoutingTable.FindClosestContacts(target.ID, alpha) ...)
+
 
   // send parallel, async FIND_NODE to the alpha (3) nodes chosen
 	findNode := 0	// amount of current findNode calls
-	findNodeResponses := 0
+	findNodeResponses := 0 // recieved responses
 
   for _, contact := range returned_contacts {
 		findNode++
 		seenIDs = append(seenIDs, contact.ID)
-    go network.SendFindContactMessage(contact, target, done)
+    go kademlia.Network.SendFindContactMessage(&contact, target, done)
   }
 
 	// if a round of FIND_NODES fails to return a node any closer than the closest
@@ -48,10 +44,10 @@ func (kademlia *Kademlia) LookupContact(target *Contact) {
 
 		for _, node := range nodes {
 			// checks if node has already been seen, if so, ignore
-			if !contains(seenIDs.String(), node.ID.String()) {
-				routingTable.AddContact(node)
+			if !contains(seenIDs, node.ID) {
+				kademlia.RoutingTable.AddContact(node)
 				returned_contacts = append(returned_contacts, node)
-				seenIDs = append(seenIDs, node.ID)
+				//seenIDs = append(seenIDs, node.ID)
 			}
 		}
 
@@ -59,13 +55,16 @@ func (kademlia *Kademlia) LookupContact(target *Contact) {
 		for findNode < alpha && findNodeResponses > k{
 
 			// sends new RPC to the closest contact in the priority queue that has not been seen
-			contacts := routingTable.FindClosestContacts(target, k)
+			contacts := kademlia.RoutingTable.FindClosestContacts(target.ID, k)
+			// TODO: find closest contact from returned_contacts NOT the routingTable
+			// CalcDistance to target, Sort
+			//contacts := returned_contacts.Sort()
 
 			// creates async FIND_NODE to the first unseen contact in the routingTable
 			for _, contact := range contacts{
-				if !contains(seenIDs.String(), contact.ID){
+				if !contains(seenIDs, contact.ID){
 					findNode++
-					go network.SendFindContactMessage(contact, target, done)
+					go kademlia.Network.SendFindContactMessage(&contact, target, done)
 					break
 				}
 			}
@@ -78,8 +77,9 @@ func (kademlia *Kademlia) LookupContact(target *Contact) {
 
 // helper function for finding out if an element exists in a slice
 // from stackoverflow https://stackoverflow.com/questions/10485743/contains-method-for-a-slice
-func contains(slice []string, item string) bool {
-    set := make(map[string]struct{}, len(slice))
+// modified to take *KademliaID instead of string
+func contains(slice []*KademliaID, item *KademliaID) bool {
+    set := make(map[*KademliaID]struct{}, len(slice))
     for _, s := range slice {
         set[s] = struct{}{}
     }
